30a14e73422a57ce7d1b9ccc2b4998af821a7d3c
Lennart Wachowiak <lennart.wachowiak@gmail.com>
2023-11-26 14:50:13 +0000

    changing platform, stretch to buster, tensorflow, ray

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
             "matplotlib",
             "requests",
             "seaborn==0.9.0",
-            "ray[rllib]",
+            "ray[rllib]==2.0.0",
             "protobuf",
             "tensorflow==2.10",
         ]
